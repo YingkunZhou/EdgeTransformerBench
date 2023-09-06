0d09e2172e9140324e4ff50599ad1643fb182c6c
YingkunZhou <zhouyingkun19@163.com>
2023-09-06 20:30:47 +0800

    upgrade nnapi in tflite_perf

--- tflite_perf.cpp
+++ tflite_perf.cpp
@@ -238,8 +238,12 @@ int main(int argc, char* argv[])
         else if (backend == 'n') {
             // https://community.nxp.com/t5/i-MX-Processors/how-to-know-the-imx8m-plus-NPU-acceleration-is-enable-already/m-p/1305328
             // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/tools/evaluation/utils.cc#L106C42-L106C42
-            delegate = NnApiDelegate();
-            if (interpreter->ModifyGraphWithDelegate(delegate) != kTfLiteOk) return -1;
+            // https://zenn.dev/iwatake2222/scraps/05e60dd7338294
+            StatefulNnApiDelegate::Options options;
+            // options.execution_preference = tflite::StatefulNnApiDelegate::Options::kSustainedSpeed;
+            // options.disallow_nnapi_cpu = true;
+            // options.allow_fp16 = true;
+            if (interpreter->ModifyGraphWithDelegate(new StatefulNnApiDelegate(options)) != kTfLiteOk) return -1;
         }
         else {
             interpreter->AllocateTensors();
