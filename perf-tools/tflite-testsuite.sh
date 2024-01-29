cc471b8ed354a1d9deed3980bc2b104f7978ebe2
YingkunZhou <zhouyingkun19@163.com>
2024-01-29 21:29:42 +0800

    update perf-tools/tflite-testsuite.sh to adapt ARMNN memory leakage on small memory devices

--- perf-tools/tflite-testsuite.sh
+++ perf-tools/tflite-testsuite.sh
@@ -2,21 +2,9 @@ download_model()
 {
     if [ ! -d ".tflite" ]
     then
-        if [ ! -f "tflite-fp32-models.tar.gz" ]
-        then
-            echo "download tflite-fp32-models.tar.gz"
-            gdown 1GIC-Q5OMuNZEh5wWM2qvZriboL_Xiov- # tflite-fp32-models.tar.gz
-        fi
-        if [ ! -f "tflite-onnx-models.tar.gz" ]
-        then
-            echo "download tflite-onnx-models.tar.gz"
-            gdown 18ZJBnwhBLac9mprisHIM_9D2w0mSXHog # tflite-onnx-models.tar.gz
-        fi
-        if [ ! -f "tflite-tinynn-models.tar.gz" ]
-        then
-            echo "download tflite-tinynn-models.tar.gz"
-            gdown 1q5gdahzHoBQSSGXEKjPsybFgHg-yOSbR # tflite-tinynn-models.tar.gz
-        fi
+        [ -f "tflite-fp32-models.tar.gz" ] && wget tflite-fp32-models.tar.gz
+        [ -f "tflite-onnx-models.tar.gz" ] && wget tflite-onnx-models.tar.gz
+        [ -f "tflite-tinynn-models.tar.gz" ] && wget tflite-tinynn-models.tar.gz
         tar xf tflite-fp32-models.tar.gz; \
         tar xf tflite-onnx-models.tar.gz; \
         tar xf tflite-tinynn-models.tar.gz
@@ -61,7 +49,7 @@ testsuite()
     echo " "
 }
 
-testsuite_onebyone()
+testsuite_mobilevitv2()
 {
     cd .tflite; rm -rf *.tflite; ln -sf $1/* .; cd ..
     BACK=$2 FP=$3 THREADS=$4 MODEL=orm make run-tflite-perf 2>/dev/null
@@ -81,18 +69,34 @@ testsuite_onebyone()
 }
 
 # under 2GB memory
-testsuite_low_memory()
+testsuite_onebyone()
 {
     cd .tflite; rm -rf *.tflite; ln -sf $1/* .; cd ..
-    BACK=$2 FP=$3 THREADS=$4 MODEL=orm make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=EMO make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=sma make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=2_0 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=2_1 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=2_2 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=LeV make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=s0 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=s1 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=s2 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=XS make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=_S make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=L1 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=1M make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=2M make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=5M make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=6M make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=ed make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=050 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=075 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=100 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=125 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=150 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=175 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=200 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=it_x make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=it_x make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=it_s make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=128 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=196 make run-tflite-perf 2>/dev/null
+    BACK=$2 FP=$3 THREADS=$4 MODEL=256 make run-tflite-perf 2>/dev/null
     BACK=$2 FP=$3 THREADS=$4 MODEL=res make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=e_1 make run-tflite-perf 2>/dev/null
     BACK=$2 FP=$3 THREADS=$4 MODEL=tf_ make run-tflite-perf 2>/dev/null
     echo " "
 }
@@ -107,10 +111,10 @@ CPU_testsuite()
     testsuite tinynn-32 z 32 $1
 
     echo ">>>>>>>>>>>armnn CPU: tfconvert fp32 model + fp32 arith<<<<<<<<<"
-    testsuite_low_memory fp32 a 32 $1
+    testsuite_onebyone fp32 a 32 $1
 
     echo ">>>>>>>>>>>armnn CPU: tinynn fp32 model + fp32 arith<<<<<<<<<"
-    testsuite_low_memory tinynn-32 a 32 $1
+    testsuite_onebyone tinynn-32 a 32 $1
 
     ### fp16
     if cat /proc/cpuinfo | grep asimdhp
@@ -122,10 +126,10 @@ CPU_testsuite()
         testsuite tinynn-32 x 16 $1
 
         echo ">>>>>>>>>>>armnn CPU: tfconvert fp32 model + fp16 arith<<<<<<<<<"
-        testsuite_low_memory fp32 a 16 $1
+        testsuite_onebyone fp32 a 16 $1
 
         echo ">>>>>>>>>>>armnn CPU: tinynn fp32 model + fp16 arith<<<<<<<<<"
-        testsuite_low_memory tinynn-32 a 16 $1
+        testsuite_onebyone tinynn-32 a 16 $1
     fi
 
     ### int8
@@ -133,7 +137,7 @@ CPU_testsuite()
     testsuite int8 z 32 $1
 
     echo ">>>>>>>>>>>xnnpack: tinynn dynamic int8 model<<<<<<<<<"
-    testsuite_onebyone tinynn-d8 z 32 $1
+    testsuite_mobilevitv2 tinynn-d8 z 32 $1
 
     echo ">>>>>>>>>>>armnn CPU: tfconvert ptq static int8 model<<<<<<<<<"
     testsuite int8 a 32 $1
@@ -149,10 +153,10 @@ GPU_testsuite()
     testsuite tinynn-32 g 32 1
 
     echo ">>>>>>>>>>>armnn GPU: tfconvert fp32 model + fp32 arith<<<<<<<<<"
-    testsuite_low_memory fp32 m 32 1
+    testsuite_onebyone fp32 m 32 1
 
     echo ">>>>>>>>>>>armnn GPU: tinynn fp32 model + fp32 arith<<<<<<<<<"
-    testsuite_low_memory tinynn-32 m 32 1
+    testsuite_onebyone tinynn-32 m 32 1
 
     ### fp16
     # make sure all opencl/gpu support fp16
@@ -163,12 +167,13 @@ GPU_testsuite()
     testsuite tinynn-32 g 16 1
 
     echo ">>>>>>>>>>>armnn GPU: tfconvert fp32 model + fp16 arith<<<<<<<<<"
-    testsuite_low_memory fp32 m 16 1
+    testsuite_onebyone fp32 m 16 1
 
     echo ">>>>>>>>>>>armnn GPU: tinynn fp32 model + fp16 arith<<<<<<<<<"
-    testsuite_low_memory tinynn-32 m 16 1
+    testsuite_onebyone tinynn-32 m 16 1
 
     ### int8
+    #TODO: use 16 is better or not?
     echo ">>>>>>>>>>>gpu: tfconvert dynamic int8 model<<<<<<<<<"
     testsuite dynamic g 32 1
 
@@ -176,7 +181,7 @@ GPU_testsuite()
     testsuite int8 g 32 1
 
     echo ">>>>>>>>>>>gpu: tinynn dynamic int8 model<<<<<<<<<"
-    testsuite_onebyone tinynn-d8 g 32 1
+    testsuite_mobilevitv2 tinynn-d8 g 32 1
 
     echo ">>>>>>>>>>>armnn CPU: tfconvert ptq static int8 model<<<<<<<<<"
     testsuite int8 m 32 1
