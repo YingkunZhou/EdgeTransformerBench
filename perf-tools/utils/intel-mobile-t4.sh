558158d93ec1919597d0ceed1045424826deda66
YingkunZhou <zhouyingkun@163.com>
2024-11-20 02:27:32 +0800

    fix benchmark setting script utils

--- perf-tools/utils/intel-mobile-t4.sh
+++ perf-tools/utils/intel-mobile-t4.sh
@@ -6,7 +6,7 @@ do
   echo 3200000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq
   echo performance > /sys/devices/system/cpu/cpufreq/policy$i/scaling_governor
 done
-for i in {4..8}
+for i in {4..7}
 do
   echo 3000000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_min_freq
   echo 3000000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq
--- perf-tools/utils/intel-mobile.sh
+++ perf-tools/utils/intel-mobile.sh
@@ -6,7 +6,7 @@ do
   echo 4400000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq
   echo performance > /sys/devices/system/cpu/cpufreq/policy$i/scaling_governor
 done
-for i in {4..8}
+for i in {4..7}
 do
   echo 3600000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_min_freq
   echo 3600000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq
