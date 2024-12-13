c6770e54e6eaa54f33da70271ecf2b61f45dc246
YingkunZhou <zhouyingkun@163.com>
2024-12-13 19:15:06 +0800

    upgrade tflite-testsuite.sh from tf-onnx-convert to google official litert

--- perf-tools/tflite-testsuite.sh
+++ perf-tools/tflite-testsuite.sh
@@ -3,9 +3,11 @@ download_model()
 {
     if [ ! -d ".tflite" ]
     then
-        [ ! -f "tflite-onnx-models.tar.gz" ]   && gdown 18ZJBnwhBLac9mprisHIM_9D2w0mSXHog
+        # [ ! -f "tflite-onnx-models.tar.gz" ]   && gdown 18ZJBnwhBLac9mprisHIM_9D2w0mSXHog
+        [ ! -f "tflite-litert-models.tar.gz" ] && gdown 14NTdkYJsT7YOtfA7OZXeEf2DoUmFq0rw
         [ ! -f "tflite-tinynn-models.tar.gz" ] && gdown 1q5gdahzHoBQSSGXEKjPsybFgHg-yOSbR
-        tar xf tflite-onnx-models.tar.gz; \
+        # tar xf tflite-onnx-models.tar.gz; \
+        tar xf tflite-litert-models.tar.gz; \
         tar xf tflite-tinynn-models.tar.gz
         if vulkaninfo | grep -q Mali
         then
@@ -67,13 +69,6 @@ download_library()
     cd ..
 }
 
-testsuite()
-{
-    cd .tflite; rm -rf *.tflite; ln -sf $1/*.tflite .; rm LeViT_256*  mobilevitv2_1[257]*  mobilevitv2_200*  tf_efficientnetv2_b3*; cd ..
-    BACK=$2 FP=$3 THREADS=$4 MODEL=ALL make run-tflite-perf 2>/dev/null
-    echo " "
-}
-
 # for tinynn dynamic int8 model
 testsuite_mobilevitv2()
 {
@@ -103,106 +98,69 @@ testsuite_series()
     echo " "
 }
 
-# for below 2GB memory device GPU armnn inference
-testsuite_onebyone()
-{
-    cd .tflite; rm -rf *.tflite; ln -sf $1/*.tflite .; rm LeViT_256* tf_efficientnetv2_b3*; cd ..
-    BACK=$2 FP=$3 THREADS=$4 MODEL=efficientformerv2_s0 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=efficientformerv2_s1 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=efficientformerv2_s2 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=SwiftFormer_XS make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=SwiftFormer_S  make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=SwiftFormer_L1 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=EMO_1M make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=EMO_2M make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=EMO_5M make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=EMO_6M make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=edgenext_xx_small make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=edgenext_x_small  make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=edgenext_small    make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilevitv2_050 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilevitv2_075 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilevitv2_100 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilevit_xx_small make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilevit_x_small  make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilevit_small    make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=LeViT_128 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=LeViT_192 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=resnet50 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=mobilenetv3_large_100 make run-tflite-perf 2>/dev/null
-    BACK=$2 FP=$3 THREADS=$4 MODEL=tf_efficientnetv2 make run-tflite-perf 2>/dev/null
-    echo " "
-}
-
 CPU_testsuite()
 {
     ### fp32
-    echo ">>>>>>>>>>>xnnpack: tfconvert fp32/fp16 model + fp32 arith<<<<<<<<<"
-    testsuite fp16 z 32 $1
+    echo ">>>>>>>>>>>xnnpack: litert fp32/fp16 model + fp32 arith<<<<<<<<<"
+    testsuite_series litert z 32 $1
 
     echo ">>>>>>>>>>>xnnpack: tinynn fp32 model + fp32 arith<<<<<<<<<"
-    testsuite tinynn-32 z 32 $1
+    testsuite_series tinynn-32 z 32 $1
+
+    if uname -m | grep -q aarch64
+    then
+        echo ">>>>>>>>>>>xnnpack: litert fp32/fp16 model + fp32 arith<<<<<<<<<"
+        testsuite_series litert a 32 $1
+
+        echo ">>>>>>>>>>>xnnpack: tinynn fp32 model + fp32 arith<<<<<<<<<"
+        testsuite_series tinynn-32 a 32 $1
+    fi
 
     ### fp16
     if cat /proc/cpuinfo | grep -q asimdhp
     then
-        echo ">>>>>>>>>>>xnnpack: tfconvert fp32/fp16 model + fp16 arith<<<<<<<<<"
-        testsuite fp16 x 16 $1
+        echo ">>>>>>>>>>>xnnpack: litert fp32/fp16 model + fp16 arith<<<<<<<<<"
+        testsuite_series litert x 16 $1
 
         echo ">>>>>>>>>>>xnnpack: tinynn fp32 model + fp16 arith<<<<<<<<<"
-        testsuite tinynn-32 x 16 $1
+        testsuite_series tinynn-32 x 16 $1
+
+        echo ">>>>>>>>>>>xnnpack: litert fp32/fp16 model + fp16 arith<<<<<<<<<"
+        testsuite_series litert a 16 $1
+
+        echo ">>>>>>>>>>>xnnpack: tinynn fp32 model + fp16 arith<<<<<<<<<"
+        testsuite_series tinynn-32 a 16 $1
     fi
 
-    ### int8
-    echo ">>>>>>>>>>>xnnpack: tfconvert PTQ static int8 model<<<<<<<<<"
-    testsuite int8 z 32 $1
-
     echo ">>>>>>>>>>>xnnpack: tinynn dynamic int8 model<<<<<<<<<"
+    # only run on xnnpack
     testsuite_mobilevitv2 tinynn-d8 z 32 $1
-
-    ##### armnn performance is not good on CPU
-    # echo ">>>>>>>>>>>armnn CPU: tfconvert fp32 model + fp32 arith<<<<<<<<<"
-    # testsuite_series fp32 a 32 $1
-
-    # echo ">>>>>>>>>>>armnn CPU: tinynn fp32 model + fp32 arith<<<<<<<<<"
-    # testsuite_series tinynn-32 a 32 $1
-
-    # if cat /proc/cpuinfo | grep -q asimdhp
-    # then
-    #     echo ">>>>>>>>>>>armnn CPU: tfconvert fp32 model + fp16 arith<<<<<<<<<"
-    #     testsuite_series fp32 a 16 $1
-
-    #     echo ">>>>>>>>>>>armnn CPU: tinynn fp32 model + fp16 arith<<<<<<<<<"
-    #     testsuite_series tinynn-32 a 16 $1
-    # fi
-
-    # echo ">>>>>>>>>>>armnn CPU: tfconvert ptq static int8 model<<<<<<<<<"
-    # testsuite int8 a 32 $1
 }
 
 GPU_testsuite()
 {
     ### fp32
-    echo ">>>>>>>>>>>gpu: tfconvert fp32/fp16 model + fp32 arith<<<<<<<<<"
-    testsuite fp16 g 32 1
+    echo ">>>>>>>>>>>gpu: litert fp32/fp16 model + fp32 arith<<<<<<<<<"
+    testsuite_series litert g 32 1
 
     echo ">>>>>>>>>>>gpu: tinynn fp32 model + fp32 arith<<<<<<<<<"
     testsuite_series tinynn-32 g 32 1
+
     if vulkaninfo | grep -q Adreno
     then
         echo "armnn GPU backend is not suitable for Qualcomm Adreno GPU"
     else
-        echo ">>>>>>>>>>>armnn GPU: tfconvert fp32 model + fp32 arith<<<<<<<<<"
-        testsuite_onebyone fp32 m 32 1
+        echo ">>>>>>>>>>>armnn GPU: litert fp32 model + fp32 arith<<<<<<<<<"
+        testsuite_series litert m 32 1
 
         echo ">>>>>>>>>>>armnn GPU: tinynn fp32 model + fp32 arith<<<<<<<<<"
-        testsuite_onebyone tinynn-32 m 32 1
+        testsuite_series tinynn-32 m 32 1
     fi
 
     ### fp16
     # make sure all opencl/gpu support fp16
-    echo ">>>>>>>>>>>gpu: tfconvert fp32/fp16 model + fp16 arith<<<<<<<<<"
-    testsuite fp16 g 16 1
+    echo ">>>>>>>>>>>gpu: litert fp32/fp16 model + fp16 arith<<<<<<<<<"
+    testsuite_series litert g 16 1
 
     echo ">>>>>>>>>>>gpu: tinynn fp32 model + fp16 arith<<<<<<<<<"
     testsuite_series tinynn-32 g 16 1
@@ -211,45 +169,25 @@ GPU_testsuite()
     then
         echo "armnn GPU backend is not suitable for Qualcomm Adreno GPU"
     else
-        echo ">>>>>>>>>>>armnn GPU: tfconvert fp32 model + fp16 arith<<<<<<<<<"
-        testsuite_onebyone fp32 m 16 1
+        echo ">>>>>>>>>>>armnn GPU: litert fp32 model + fp16 arith<<<<<<<<<"
+        testsuite_series litert m 16 1
 
         echo ">>>>>>>>>>>armnn GPU: tinynn fp32 model + fp16 arith<<<<<<<<<"
-        testsuite_onebyone tinynn-32 m 16 1
+        testsuite_series tinynn-32 m 16 1
     fi
 
     ### int8
-    echo ">>>>>>>>>>>gpu: tfconvert PTQ static int8 model<<<<<<<<<"
-    testsuite int8 g 16 1
-
     echo ">>>>>>>>>>>gpu: tinynn dynamic int8 model<<<<<<<<<"
     testsuite_mobilevitv2 tinynn-d8 g 16 1
-
-    if vulkaninfo | grep -q Adreno
-    then
-        echo "armnn GPU backend is not suitable for Qualcomm Adreno GPU"
-    else
-        echo ">>>>>>>>>>>armnn GPU: tfconvert ptq static int8 model<<<<<<<<<"
-        testsuite_series int8 m 32 1
-    fi
-
-    echo ">>>>>>>>>>>gpu: tfconvert dynamic int8 model<<<<<<<<<"
-    testsuite dynamic g 16 1
 }
 
 NNAPI_testsuite()
 {
-    echo ">>>>>>>>>>>nnapi: tfconvert fp32/fp16 model<<<<<<<<<"
-    testsuite fp16 n 32 1
+    echo ">>>>>>>>>>>nnapi: litert fp32 model<<<<<<<<<"
+    testsuite_series litert n 32 1
 
     echo ">>>>>>>>>>>nnapi: tinynn fp32 model<<<<<<<<<"
-    testsuite tinynn-32 n 32 1
-
-    echo ">>>>>>>>>>>nnapi: tfconvert dynamic int8 model<<<<<<<<<"
-    testsuite dynamic n 32 1
-
-    echo ">>>>>>>>>>>nnapi: tfconvert PTQ static int8 model<<<<<<<<<"
-    testsuite int8 n 32 1
+    testsuite_series tinynn-32 n 32 1
 
     echo ">>>>>>>>>>>nnapi: tinynn dynamic int8 model<<<<<<<<<"
     testsuite_mobilevitv2 tinynn-d8 n 32 1
